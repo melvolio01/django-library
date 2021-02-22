dfe3308ccf4f2804aaa4e468a988f172c621194e
Melvolio01 <andyhunt353@gmail.com>
2021-02-22 21:27:59 +0000

    Updating admin models

--- catalogue/models.py
+++ catalogue/models.py
@@ -28,6 +28,12 @@ class Book(models.Model):
 
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
 
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+
+    display_genre.short_description = 'Genre'
+
     def __str__(self):
         return self.title
 
