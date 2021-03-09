5c585c597f07fa152a23a32a916cf2f86245c344
Melvolio01 <andyhunt353@gmail.com>
2021-03-09 19:20:00 +0000

    Prepping for deployment

--- django_library/settings.py
+++ django_library/settings.py
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#kwgui3ja)ygtyuk#ry6k%!fp#k@i9o02@a!y(r_z_lnz!1)ia'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
